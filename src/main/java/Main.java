94941aa2b1cefeb2856159a63e71ebade5842b9f
Giskard <paldenpromo@gmail.com>
2021-11-23 11:06:20 +0100

    A Main 11. sorának kikommentelésével lefut.

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+package main.java;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> furcsaLista = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println(furcsaLista.getClass());
+        furcsaLista.remove(2);
+    }
+
+}
